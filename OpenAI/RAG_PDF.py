79ec6917ca450e66185fcb394f6cd4ab521acfe8
Tarini1103 <tarinijhamb@gmail.com>
2024-09-08 19:01:52 +0530

    Update and rename RAG_openAI_PDF.py to OpenAI/RAG_PDF.py

--- OpenAI/RAG_PDF.py
+++ OpenAI/RAG_PDF.py
@@ -11,7 +11,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 # Initialize the Azure OpenAI client
 httpx_client = httpx.Client(verify=False)
 openai_client = AzureOpenAI(
-    api_key="d2323ce0232940d98649e66d446ddec3",
+    api_key="API_KE",
     api_version="2023-09-15-preview",
     azure_endpoint="https://newopenairnd.openai.azure.com/",
     http_client=httpx_client
